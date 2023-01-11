9429dc5c1f37aac3cf617a965a9f4701563e511d
Mr. Kartik Saini <104669343+Iamkartiksaini@users.noreply.github.com>
2023-01-11 09:26:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { Provider } from "react-redux";
-import Button from "./button";
-import Header from "./Header";
-import store from "./Redux/store";
+
 function App() {
   return (
-    <Provider store={store}>
-      <h1>Welcome to React Redux</h1>
-      <Header />
-      <Button />
-    </Provider>
+   <> <h1> Welcome to React </h1></>
   );
 }
 
